cbacabdf2466e56502fcd6201ea15729085c1e5b
olwend <olwend@gmail.com>
2017-03-07 21:34:49 +0000

    Added missing boundary cases texttest_fixture.rb

--- gildedrose/spec/gilded_rose_spec.rb
+++ gildedrose/spec/gilded_rose_spec.rb
@@ -3,33 +3,40 @@ require_relative '../gilded_rose'
 describe GildedRose do
 
   describe "item" do
-    it "has a SellIn value showing number of days till expiry" do
+
+    it "has a sell_in value showing number of days till expiry" do
     end
 
-    it "has a Quality value showing a monetary value" do
+    it "has a quality value" do
     end
 
-    it "only has a positive Quality value" do
+    it "only has a positive quality value" do
     end
 
-    it "has a Quality value <= 50" do
+    it "has a quality value <= 50" do
     end
 
-    it "has a Sell by date which can be nil" do
+    it "when sell_in value is zero, it has reached sell_by_date" do
     end
 
     it "has a quality change ratio (x) which can be positve or negative" do
     end
   end
 
+  describe "Sulfuras legendary item" do
+    it "has a sell_in date of nil so never decreases in quality" do
+    end
+    it "has a quality value of 80" do
+    end
+
   describe "Backstage passes item" do
-    it "increases Quality value by 2 when sell by date - today is <= 10" do
+    it "increases quality value by 2 when sell by date - today is <= 10" do
     end
 
-    it "increases Quality value by 3 when sell by date - today is <= 5" do
+    it "increases quality value by 3 when sell by date - today is <= 5" do
     end
 
-    it "has a Quality value = 0 after concert" do
+    it "has a quality value = 0 after concert" do
     end
   end
 
@@ -40,16 +47,15 @@ describe GildedRose do
     end
   end
 
-
-
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("fixme", 0, 0)]
+      items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+
+      expect(items[0].name).to eq "foo"
     end
 
-    it "lowers item SellIn value and Quality value daily" do
+    it "lowers item sell_in and quality daily" do
     end
 
     it "lowers value by x when today < sell by date" do
