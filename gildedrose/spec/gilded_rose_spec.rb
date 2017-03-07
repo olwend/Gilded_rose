44a5382a46f3650d3ba514399db8189c99e11d50
olwend <olwend@gmail.com>
2017-03-07 13:18:25 +0000

    Updated tests to describe requirements

--- gildedrose/spec/gilded_rose_spec.rb
+++ gildedrose/spec/gilded_rose_spec.rb
@@ -1,6 +1,60 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require_relative '../gilded_rose'
 
 describe GildedRose do
+
+  describe "item" do
+    it "has a SellIn value showing number of days till expiry" do
+    end
+
+    it "has a Quality value showing a monetary value" do
+    end
+
+    it "only has a positive Quality value" do
+    end
+
+    it "has a Quality value <= 50" do
+    end
+
+    it "has a Sell by date which can be nil" do
+    end
+
+    it "has a quality change ratio (x) which can be positve or negative" do
+    end
+  end
+
+  describe "update_Quality_method" do
+    it "lowers item SellIn value and Quality value daily" do
+    end
+
+    it "lowers value by x when today < sell by date" do
+    end
+
+    it "lowers value by 2x when today > sell by date" do
+    end
+
+    it "uses different x values (change ratio) per item" do
+    end
+  end
+
+  describe "Backstage passes item" do
+    it "increases Quality value by 2 when sell by date - today is <= 10" do
+    end
+
+    it "increases Quality value by 3 when sell by date - today is <= 5" do
+    end
+
+    it "has a Quality value = 0 after concert" do
+    end
+  end
+
+  describe "conjured items" do
+    it "lowers value by 2x when today < sell by date" do
+    end
+    it "lowers value by 4x when today > sell by date" do
+    end
+  end
+
+
 
   describe "#update_quality" do
     it "does not change the name" do
@@ -8,6 +62,8 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "fixme"
     end
+
+    it ""
   end
 
 end
